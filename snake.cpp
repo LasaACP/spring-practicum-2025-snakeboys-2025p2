9881faaaa8296093769ff086646eed685cef50bb
Henry <liuhenry08@gmail.com>
2025-05-19 10:52:31 -0500

    fixed bugs and stuff pt3

--- snake.cpp
+++ snake.cpp
@@ -2,12 +2,12 @@
 
 snake::snake(Position start){
     body.push_back(start);
-    int init_direct = {1, 0};
+    init_direct = {1, 0};
 }
 
 void snake::move(bool incr){
     Position head = body.front();
-    Position nHead = {head.x+init_direct.x, head.y, +init_direct.y};
+    Position nHead = {head.x+init_direct.x, head.y+ init_direct.y};
     body.push_front(nHead);
     if (!incr){
         body.pop_back();
--- snake.h
+++ snake.h
@@ -1,8 +1,18 @@
 #ifndef SNAKE_H
 #define SNAKE_H
+#include <list>
+using namespace std;
 
+struct Position{
+    int x, y;
+    bool operator==(Position& temp){
+        return temp.x == temp.y;
+    } 
+};
 class snake{
     public:
+        list<Position> body;
+        Position init_direct;
         snake(Position start);
         void move(bool incr);
 };
