f153005f96aea15ef3b80ab5c82e5173653a5c3b
Henry <liuhenry08@gmail.com>
2025-05-19 09:49:56 -0500

    fixed bugs

--- snake.cpp
+++ snake.cpp
@@ -2,14 +2,14 @@
 
 snake::snake(Position start){
     body.push_back(start);
-    init_direct = {1, 0};
+    int init_direct = {1, 0};
 }
 
 void snake::move(bool incr){
-    Postion head = body.front();
+    Position head = body.front();
     Position nHead = {head.x+init_direct.x, head.y, +init_direct.y};
-    body.push_front(newHead);
-    if (!grow){
+    body.push_front(nHead);
+    if (!incr){
         body.pop_back();
     }
 }
